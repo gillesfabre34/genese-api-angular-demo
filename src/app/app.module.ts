6ae0d6531b760dfd124a77a286a2692b4f3838db
Gilles Fabre <gillesfabrejob@gmail.com>
2020-03-23 21:41:57 +0100

    Refactored folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { HomeModule } from './demo/home/home.module';
+import { HomeModule } from './features/home.module';
 import { GeneseModule } from 'genese-angular';
 
 @NgModule({
