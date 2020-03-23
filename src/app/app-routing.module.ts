c4f9d2ee486aa1935e0ecc0921f91c6a8994f770
Gilles Fabre <gillesfabrejob@gmail.com>
2020-03-23 21:41:57 +0100

    Refactored folders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './demo/home/home.component';
-import { AuthGuardService } from './demo/core/services/auth-guard.service';
-import { GetOneComponent } from './demo/home/get-one/get-one.component';
-import { GetOneCustomComponent } from './demo/home/get-one-custom/get-one-custom.component';
-import { GetAllComponent } from './demo/home/get-all/get-all.component';
-import { GetAllCustomComponent } from './demo/home/get-all-custom/get-all-custom.component';
-import { CreateComponent } from './demo/home/create/create.component';
-import { CreateCustomComponent } from './demo/home/create-custom/create-custom.component';
-import { DeleteComponent } from './demo/home/delete/delete.component';
-import { DeleteCustomComponent } from './demo/home/delete-custom/delete-custom.component';
-import { UpdateComponent } from './demo/home/update/update.component';
-import { UpdateCustomComponent } from './demo/home/update-custom/update-custom.component';
-import { WelcomeComponent } from './demo/home/welcome/welcome.component';
-import { GetArrayComponent } from './demo/home/get-array/get-array.component';
+import { HomeComponent } from './features/home.component';
+import { AuthGuardService } from './core/services/auth-guard.service';
+import { GetOneComponent } from './features/get-one/get-one.component';
+import { GetOneCustomComponent } from './features/get-one-custom/get-one-custom.component';
+import { GetAllComponent } from './features/get-all/get-all.component';
+import { GetAllCustomComponent } from './features/get-all-custom/get-all-custom.component';
+import { CreateComponent } from './features/create/create.component';
+import { CreateCustomComponent } from './features/create-custom/create-custom.component';
+import { DeleteComponent } from './features/delete/delete.component';
+import { DeleteCustomComponent } from './features/delete-custom/delete-custom.component';
+import { UpdateComponent } from './features/update/update.component';
+import { UpdateCustomComponent } from './features/update-custom/update-custom.component';
+import { WelcomeComponent } from './features/welcome/welcome.component';
+import { GetArrayComponent } from './features/get-array/get-array.component';
 
 
 const routes: Routes = [
@@ -35,7 +35,7 @@ const routes: Routes = [
             {path: 'update-custom', component: UpdateCustomComponent, canActivate: [AuthGuardService]},
         ]
     },
-    { path: 'home', component: HomeComponent, canActivate: [AuthGuardService] },
+    { path: 'features', component: HomeComponent, canActivate: [AuthGuardService] },
     { path: '**', component: HomeComponent, data: { error: 'Not found' }}
 ];
 
