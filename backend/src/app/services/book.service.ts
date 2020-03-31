2e51451c625e0c262095d6a3b737799db855bf86
Gilles Fabre <gillesfabrejob@gmail.com>
2020-03-31 09:02:22 +0200

    Put() runs in backend

--- backend/src/app/services/book.service.ts
+++ backend/src/app/services/book.service.ts
@@ -3,6 +3,11 @@ import { BOOKS } from '../mocks/book.mock';
 import { Book } from '../models/book.model';
 import { GenericDataService } from '../../generic/services/generic-data.service';
 import chalk from 'chalk';
+<<<<<<< Updated upstream
+=======
+import { BookPost } from '../../../../genese/genese-api/datatypes/book-post.datatype';
+import { BookPut } from '../../../../genese/genese-api/datatypes/book-put.datatype';
+>>>>>>> Stashed changes
 
 @Injectable()
 export class BookService extends GenericDataService<Book> {
@@ -36,5 +41,39 @@ export class BookService extends GenericDataService<Book> {
             resolve(this.books);
         });
     }
+<<<<<<< Updated upstream
+=======
+
+
+    /**
+     * Update a book
+     * @param bookID
+     * @param bookPut
+     */
+    updateBook(bookID: string, bookPut: BookPut): Promise<any> {
+        if (!bookID || !bookPut) {
+            throw new HttpException('BookId or body does not exist!', 404);
+        }
+        const id = Number(bookID);
+        return new Promise(resolve => {
+            const index = this.books.findIndex(book => book.id === id);
+            if (!index) {
+                throw new HttpException('Book does not exist!', 404);
+            }
+            const updatedBook = bookPut as Book;
+            updatedBook.id = this.books[index].id;
+            this.books[index] = updatedBook;
+            resolve(updatedBook);
+        });
+    }
+
+
+
+    getNewId(): number {
+        const newId = Math.max(...this.books.map(e => e.id)) + 1;
+        console.log('newId', newId);
+        return newId;
+    }
+>>>>>>> Stashed changes
 }
 
